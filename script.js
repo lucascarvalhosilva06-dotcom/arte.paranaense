47afdcec0a5d5c65ae21d60764d3628672558748
lucascarvalhosilva06-dotcom <lucas.carvalho.silva06@escola.pr.gov.br>
2026-09-25 08:20:34 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySelectorA11(".curtir");
+botoesCurtir.array.forEach(function(botaoCurtir){
+let curtir=false
+botaocurtir.addEventlistener("click",curtir);
+function curtir(){
+const contador = botaoCurtir.querySelector("span");
+if(curtiu === false){
+contador.textcontent++;
+curtiu= true;}
+else{
+contador.textcontent--;
+curtiu = false;
+}
+}
+});
